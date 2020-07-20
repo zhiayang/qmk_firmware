cddfe8b5379e0a43ab284cf8d9b42aa4d695509e
zhiayang <zhiayang@nascentnoodle.com>
2020-07-20 14:55:09 +0800

    update iris keymap

--- keyboards/keebio/iris/keymaps/zhiayang/keymap.c
+++ keyboards/keebio/iris/keymaps/zhiayang/keymap.c
@@ -20,9 +20,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 	//├────────┼────────┼────────┼────────┼────────┼────────┤                      ├────────┼────────┼────────┼────────┼────────┼────────┤
 	   CST_CAPS,  KC_A  ,  KC_S  ,  KC_D  ,  KC_F  ,  KC_G  ,                         KC_H  ,  KC_J  ,  KC_K  ,  KC_L  , KC_SCLN, KC_QUOT,
 	//├────────┼────────┼────────┼────────┼────────┼────────┼────────┐    ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-	    KC_LSFT,  KC_Z  ,  KC_X  ,  KC_C  ,  KC_V  ,  KC_B  ,  NOKEY ,       NOKEY ,  KC_N  ,  KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_ENT ,
+	    KC_LSFT,  KC_Z  ,  KC_X  ,  KC_C  ,  KC_V  ,  KC_B  ,  NOKEY ,       NOKEY ,  KC_N  ,  KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
 	//└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘    └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-	                                  KC_LCTRL, KC_LALT, KC_SPC ,               KC_ENT , KC_SPC , CST_FN
+	                                  KC_LCTRL, KC_LALT, KC_SPC ,               KC_ENT , LALT(LCTL(KC_LGUI)), CST_FN
 	//                               └────────┴────────┴────────┘             └────────┴────────┴────────┘
 	),
 
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 	//├────────┼────────┼────────┼────────┼────────┼────────┤                      ├────────┼────────┼────────┼────────┼────────┼────────┤
 	   CST_CAPS,  KC_A  ,  KC_S  ,  KC_D  ,  KC_F  ,  KC_G  ,                         KC_H  ,  KC_J  ,  KC_K  ,  KC_L  , KC_SCLN, KC_QUOT,
 	//├────────┼────────┼────────┼────────┼────────┼────────┼────────┐    ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-	    KC_LSFT,  KC_Z  ,  KC_X  ,  KC_C  ,  KC_V  ,  KC_B  ,  NOKEY ,       NOKEY ,  KC_N  ,  KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_ENT ,
+	    KC_LSFT,  KC_Z  ,  KC_X  ,  KC_C  ,  KC_V  ,  KC_B  ,  NOKEY ,       NOKEY ,  KC_N  ,  KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
 	//└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘    └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
 	                                  KC_LCTRL, KC_LGUI, KC_SPC ,               KC_ENT , KC_LALT, CST_FN
 	//                               └────────┴────────┴────────┘             └────────┴────────┴────────┘
